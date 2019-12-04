89a002908de54682856960b90db7bcd5f606da2a
colbyw5 <cwilkinson@hsph.harvard.edu>
2019-12-04 06:34:15 -0500

    abstract updates

--- rl_work/puckworld.py
+++ rl_work/puckworld.py
@@ -51,7 +51,6 @@ class DQNSolver:
         self.model = Sequential()
         self.model.add(Dense(24, input_shape=(observation_space,), activation="relu"))
         self.model.add(Dense(24, activation="relu"))
-        self.model.add(Dense(24, activation="relu"))
         self.model.add(Dense(self.action_space, activation="linear"))
         self.model.compile(loss="mse", optimizer=Adam(lr=LEARNING_RATE))
 
